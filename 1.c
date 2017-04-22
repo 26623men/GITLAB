1bc20c3d62e1918f39f7cef13c58bf25912551d0
26623men <26623men@gmail.com>
2017-04-22 23:10:47 +0700

    Modify Program#1 by A

--- 1.c
+++ 1.c
@@ -1,6 +1,8 @@
 fisrt program
 
-CM Lab by Kanokphan Thananchai 57070501082
+CM Lab by Kanokphan [NUMBER18 change by user a] Thananchai 57070501082
 
 change by Order number 13
 	by User A
+
+[NUMBER18 change by user a]
